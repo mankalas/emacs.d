5b5e8355a97c8f6da889bf16943996cc664d71cc
Vincent Boucheny <mankalas@gmail.com>
2022-07-26 15:43:01 +1200

    Add fun maybe-require-package

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+
 (defun require-package (package &optional min-version no-refresh)
   "Install given PACKAGE, optionally requiring MIN-VERSION.
 If NO-REFRESH is non-nil, the available package lists will not be
@@ -26,6 +27,18 @@ re-downloaded in order to locate PACKAGE."
             (package-refresh-contents)
             (require-package package min-version t)))
         (package-installed-p package min-version))))
+
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install optional package `%s': %S" package err)
+     nil)))
 
 ;; Custom
 (setq custom-file (concat user-emacs-directory "/custom.el"))
