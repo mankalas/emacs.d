348f4ae49ccee84470125fffdf3d1425d58a4ed9
Vincent Boucheny <mankalas@gmail.com>
2023-06-29 23:15:53 -1000

    Weeks start on Monday

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,4 +1,5 @@
-(custom-set-variables '(custom-enabled-themes '(zenburn)))
+(custom-set-variables '(custom-enabled-themes '(zenburn))
+                      '(calendar-week-start-day 1))
 
 (provide 'init-local)
 ;;; init-local.el ends here
